64c84260048df46b89616056825f1ddcf9c7b4c5
thaersaidi <131191852+thaersaidi@users.noreply.github.com>
2023-10-02 17:51:59 +0200

    add

--- site-generator-ng/src/environments/environment.prod.ts
+++ site-generator-ng/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyA2wXas1gdmAzamuvI2gFM_spA28ASUzLA",
+    authDomain: "site-generator-ng.firebaseapp.com",
+    projectId: "site-generator-ng",
+    storageBucket: "site-generator-ng.appspot.com",
+    messagingSenderId: "484329035762",
+    appId: "1:484329035762:web:f9aeb62e2f8d83a0c03d6b"
+  }
+
 };
