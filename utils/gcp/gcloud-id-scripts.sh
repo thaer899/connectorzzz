c62f5703fdb09cd6371695f7542da65e0887622d
thaersaidi <131191852+thaersaidi@users.noreply.github.com>
2023-10-11 02:16:03 +0200

    updates

--- utils/gcp/gcloud-id-scripts.sh
+++ utils/gcp/gcloud-id-scripts.sh
@@ -25,8 +25,7 @@ export REPO="thaersaidi.net"
 gcloud iam service-accounts add-iam-policy-binding "thaersaidi-sa@thaersaidi-da79c.iam.gserviceaccount.com" \
   --project="thaersaidi-da79c" \
   --role="roles/iam.workloadIdentityUser" \
-  --member="principalSet://iam.googleapis.com/github-actions-oidc/attribute.repository/thaersaidi.net"
-
+  --member="principal://iam.googleapis.com/github-actions-oidc/attribute.repository_owner/thaer899"
 
 gcloud projects add-iam-policy-binding thaersaidi-da79c \
     --member="serviceAccount:thaersaidi-sa@thaersaidi-da79c.iam.gserviceaccount.com" \
@@ -45,13 +44,21 @@ gcloud iam service-accounts add-iam-policy-binding \
   --role="roles/iam.serviceAccounts.getAccessToken" \
   --project=thaersaidi-da79c
 
-gcloud iam workload-identity-pools providers describe "github-actions-oidc" \
+gcloud iam workload-identity-pools providers describe "github-actions" \
   --project="thaersaidi-da79c" \
   --location="global" \
-  --workload-identity-pool="github-actions-oidc" \
+  --workload-identity-pool="projects/250500941343/locations/global/workloadIdentityPools/github-actions/providers/github-actions-oidc" \
   --format="value(name)"
+
+gcloud iam workload-identity-pools providers list \
+  --workload-identity-pool="github-actions" \
+  --project="thaersaidi-da79c" \
+  --location="global"
 
 
 gcloud config set auth/impersonate_service_account thaersaidi-sa@thaersaidi-da79c.iam.gserviceaccount.com
 
 gcloud iam service-accounts list --project=thaersaidi-da79c
+
+
+gcloud services enable iam.googleapis.com iamcredentials.googleapis.com
