54a047b5b2e2fd48c68214185056b1b86f802607
thaersaidi <131191852+thaersaidi@users.noreply.github.com>
2023-10-18 20:16:54 +0200

    updates

--- myresume-ng/tests/home/home.spec.ts
+++ myresume-ng/tests/home/home.spec.ts
@@ -4,7 +4,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   console.log('Before navigating to the page');
 
-  await page.goto('http://localhost:4200');
+  await page.goto('https://thaersaidi.net');
   console.log('After navigating to the page');
 
   // await page.waitForSelector('app-home');
@@ -13,11 +13,11 @@ test('has title', async ({ page }) => {
 });
 
 test('go to employment page', async ({ page }) => {
-  await page.goto('http://localhost:4200');
+  await page.goto('https://thaersaidi.net');
 
   await page.click('text=Employment');
   // await page.getByRole('link', { name: 'Employment' }).click();
 
-  await expect(page).toHaveURL('http://localhost:4200/#/resume/employment');
+  await expect(page).toHaveURL('https://thaersaidi.net/employment');
 }
 );
