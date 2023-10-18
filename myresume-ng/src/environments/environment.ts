1e368b4d531e96c6a847e44727ac4ab4c87c9f04
thaersaidi <131191852+thaersaidi@users.noreply.github.com>
2023-10-18 21:46:09 +0200

    updates

--- myresume-ng/src/environments/environment.ts
+++ myresume-ng/src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  host: 'https://thaersaidi.net',
+  host: 'http://localhost:7000',
   mainEmail: "rebel.saidi.thaer@gmail.com",
   functionURL: 'https://ai.thaersaidi.net',
   message_api_key: 'FJxJ2MRv4vtAH0Js81Q390',
--- myresume-ng/tests/home/home.spec.ts
+++ myresume-ng/tests/home/home.spec.ts
@@ -6,57 +6,47 @@ const host = environment.host;
 
 
 
-// test('has title', async ({ page }) => {
-//   await page.goto(host);
-//   await expect(page).toHaveTitle(/Log Book - Home/);
-// });
+test('has title', async ({ page }) => {
+  await page.goto(host);
+  await expect(page).toHaveTitle(/Log Book - Home/);
+});
 
-// test('has resume', async ({ page }) => {
-//   await page.goto(host);
-//   await page.waitForSelector('.name-text', { state: 'visible' });
-//   const hasNameText = await page.isVisible('.name-text');
-//   expect(hasNameText).toBe(true);
-// });
+test('has resume', async ({ page }) => {
+  await page.goto(host);
+  await page.waitForSelector('.name-text', { state: 'visible' });
+  const hasNameText = await page.isVisible('.name-text');
+  expect(hasNameText).toBe(true);
+});
 
-// test('has quote', async ({ page }) => {
-//   await page.goto(host);
-//   await page.waitForSelector('.quote', { state: 'visible' });
-//   const hasNameText = await page.isVisible('.quote');
-//   expect(hasNameText).toBe(true);
-// });
+test('has quote', async ({ page }) => {
+  await page.goto(host);
+  await page.waitForSelector('.quote', { state: 'visible' });
+  const hasNameText = await page.isVisible('.quote');
+  expect(hasNameText).toBe(true);
+});
 
-// test('go to employment page', async ({ page }) => {
-//   await page.goto(host);
-//   await page.click('text=Employment');
-//   await expect(page).toHaveURL(host + '/employment');
-// });
-
-
+test('go to employment page', async ({ page }) => {
+  await page.goto(host);
+  await page.click('text=Employment');
+  await expect(page).toHaveURL(host + '/employment');
+});
 
 test('input birthday and get result', async ({ page }) => {
   await page.goto(host);
-
-  // Wait for the textarea to be visible
   await page.waitForSelector('section#ask textarea#recipientMessage', { state: 'visible' });
-
-  // Type "birthday" into the textarea
   await page.fill('section#ask textarea#recipientMessage', 'birthday');
-
-  // Refine the selector to be more specific to the "ask" section
   const button = await page.locator('section#ask button.skill-set[type="submit"]');
-
-  // Scroll the button into view (if needed) and click
   await button.scrollIntoViewIfNeeded();
-  await button.click();
-
-  // Wait for the message response to appear
+  await Promise.all([
+    page.waitForResponse(response => response.url().includes('ai.thaersaidi.net') && response.status() === 200),
+    button.click()
+  ]);
   await page.waitForSelector('#messageResponse');
-
-  // Get the content of the response
-  const messages = await page.$$eval('#messageResponse .card-text', elements => elements.map(e => e.textContent.trim()));
-
-  // Print the content of the responses
+  const messages = await page.$$eval('#messageResponse',
+    elements => elements.map(e => (e.textContent?.trim() || '')));
   console.log(messages);
+  expect(messages.length).toBeGreaterThan(0);
+  expect(messages[0]).toContain('March 5th');
 });
 
 
