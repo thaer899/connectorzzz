3010da00d365666316bcc76a464a2564fcd49520
thaersaidi <131191852+thaersaidi@users.noreply.github.com>
2023-10-20 10:52:10 +0200

    updates

--- myresume-ng/src/environments/environment.ts
+++ myresume-ng/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   host: 'https://thaersaidi.net',
   mainEmail: "rebel.saidi.thaer@gmail.com",
-  functionURL: 'https://ai.thaersaidi.net',
+  functionURL: 'https://ai.thaersaidi.net/myresume',
   message_api_key: 'FJxJ2MRv4vtAH0Js81Q390',
   title: 'Log Book',
   firebaseConfig: {
