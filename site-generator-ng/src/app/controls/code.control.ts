9098a84160ce153da6e384ede03013ab06013a3f
thaersaidi <131191852+thaersaidi@users.noreply.github.com>
2023-11-16 02:11:42 +0100

    updates

--- site-generator-ng/src/app/controls/code.control.ts
+++ site-generator-ng/src/app/controls/code.control.ts
@@ -1,35 +1,56 @@
 import { Component } from '@angular/core';
 import { JsonFormsAngularService, JsonFormsControl } from '@jsonforms/angular';
-import { ControlProps } from '@jsonforms/core';
 
 @Component({
-  selector: 'app-data-component',
+  selector: 'app-code-component',
   styles: [
     `
       :host ::ng-deep .editor-container {
         height: 300px;
       }
-      :host ::ng-deep .my-code-editor  {
+      :host ::ng-deep .my-code-editor {
         height: 250px;
         border: 0.5px solid #cdcdcd5e;
-    padding-top: 1vh;      }
+        padding-top: 1vh;
+      }
+      .toggle-language-button {
+        margin-bottom: 10px;
+      }
     `
   ],
   template: `
     <div class="editor-container">
+      <button mat-button class="toggle-language-button" (click)="toggleLanguage()">
+        Code in {{ this.currentLanguage | uppercase }}
+      </button>
       <ngx-monaco-editor class="my-code-editor" [options]="editorOptions" [(ngModel)]="code"></ngx-monaco-editor>
     </div>
   `
 })
 export class CodeComponent extends JsonFormsControl {
-
   editorOptions = { theme: 'vs-dark', language: 'python' };
-  code = 'def x():\n    print("Hello world!")\n'
+  code = 'def x():\n    print("Hello world!")\n';
+  currentLanguage: 'python' | 'yaml' = 'python';
 
   constructor(service: JsonFormsAngularService) {
     super(service);
   }
 
+  toggleLanguage() {
+    if (this.currentLanguage === 'python') {
+      this.currentLanguage = 'yaml';
+      this.editorOptions = { ...this.editorOptions, language: 'yaml' };
+      // Update the code variable if you also want to change the content of the editor
+      this.code = 'yaml: content';
+    } else {
+      this.currentLanguage = 'python';
+      this.editorOptions = { ...this.editorOptions, language: 'python' };
+      // Update the code variable if you also want to change the content of the editor
+      this.code = 'def x():\n    print("Hello world!")\n';
+    }
+  }
 
-
+  getNextLanguage() {
+    return this.currentLanguage === 'python' ? 'yaml' : 'python';
+  }
 }
